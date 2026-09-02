fa5c35f3c9e274d112d3977a245092836f200bd8
abdelrahman <abdelrahmankarim7@gmail.com>
2026-09-02 18:56:15 +0300

    feat(domain): add Hotel and User domain entities

---
+++ app/src/main/java/com/task/hotelhop/domain/entity/User.kt
@@ -0,0 +1,9 @@
+package com.task.hotelhop.domain.entity
+
+data class User(
+    val id: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val gender: String
+)
